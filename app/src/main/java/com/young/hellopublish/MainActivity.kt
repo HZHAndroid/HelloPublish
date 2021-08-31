4020ee73bd01a09ee0740d7db7076a88934f887d
蓝月羽 <1104090872@qq.com>
2021-08-31 23:17:05 +0800

    add: 1、添加了资源文件，测试资源文件是否会被发布。利用 aab 的形式。

--- app/src/main/java/com/young/hellopublish/MainActivity.kt
+++ app/src/main/java/com/young/hellopublish/MainActivity.kt
@@ -3,19 +3,19 @@ package com.young.hellopublish
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.young.hellolib.ICallback
+//import com.young.hellolib.ICallback
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-      val call = object : ICallback<String>{
-          override fun onCall(bean: String) {
-              Log.e("youngshen", "bean = $bean")
-          }
-      }
-
-        call.onCall("shen long")
+//      val call = object : ICallback<String>{
+//          override fun onCall(bean: String) {
+//              Log.e("youngshen", "bean = $bean")
+//          }
+//      }
+//
+//        call.onCall("shen long")
     }
 }
