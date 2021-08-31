3abb3bfb1a5e8a66cf199924a558ccbdb85aac85
蓝月羽 <1104090872@qq.com>
2021-08-31 21:20:00 +0800

    first commit

---
+++ hellolib/src/main/java/com/young/hellolib/ICallback.kt
@@ -0,0 +1,29 @@
+package com.young.hellolib
+
+/**
+ * @author:  Young
+ * .
+ * @email:   1104090872@qq.com
+ * .
+ * @date :   2021/8/31 21:07 周二
+ * 　　 へ　　　　　／|
+　　/＼7　　∠＿/
+　 /　│　　 ／　／
+　│　Z ＿,＜　／　　 /`ヽ
+　│　　　　　ヽ　　 /　　〉
+　 Y　　　　　`　 /　　/
+　ｲ●　､　●　　⊂⊃〈　　/
+　()　 へ　　　　|　＼〈
+　　>ｰ ､_　 ィ　 │ ／／
+　 / へ　　 /　ﾉ＜| ＼＼
+　 ヽ_ﾉ　　(_／　 │／／
+　　7　　　　　　　|／
+　　＞―r￣￣`ｰ―＿
+
+ * @description:
+ */
+interface ICallback<T> {
+
+
+    fun onCall(bean: T)
+}
